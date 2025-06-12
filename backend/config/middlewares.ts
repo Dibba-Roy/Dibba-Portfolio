08d578c974ec4312777a8957d753227fa8a19eef
Dibba-Roy <dibba.secondary@gmail.com>
2025-06-11 23:26:40 -0400

    Adding middleware to strapi

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -2,7 +2,22 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+
+  // CORS must be an object to configure it
+  {
+    name: 'strapi::cors',
+    config: {
+      // allow your front-end origin(s):
+      origin: [
+        'http://localhost:5173',            
+        'https://dibba-portfolio.pages.dev/',
+      ],
+      methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
+      headers: ['Content-Type','Authorization','Accept'],
+      keepHeaderOnError: true,
+    },
+  },
+
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
