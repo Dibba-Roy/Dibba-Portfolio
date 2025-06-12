418d5b716336c5220abeb5dbd6e9a266d7d43621
Dibba-Roy <dibba.secondary@gmail.com>
2025-06-11 21:15:08 -0400

    Updating backend + preparing for deployment

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,1 +1,17 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('CF_R2_ACCESS_KEY_ID'),
+        secretAccessKey: env('CF_R2_SECRET_ACCESS_KEY'),
+        region: env('CF_R2_REGION', 'auto'),
+        endpoint: env('CF_R2_ENDPOINT'),
+        params: { Bucket: env('CF_R2_BUCKET') },
+        signatureVersion: 'v4',
+        forcePathStyle: true,
+      },
+      actionOptions: { upload: {}, delete: {} },
+    },
+  },
+});
